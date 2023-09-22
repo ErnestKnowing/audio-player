feff409140056393e27230cc18c9109039f7cd24
ErnestKnowing <e3050901@gmail.com>
2023-09-22 12:11:47 +0300

    feat: created switing music

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const audio = document.querySelector("audio");
+const arrayTrack = ['music/bird.mp3', 'music/rain.mp3'];
+let rightSkip = document.querySelector('#right');
+let leftSkip = document.querySelector('#left');
+audio.src = 'music/bird.mp3';
+let indexArray = 0;
+rightSkip.addEventListener('click',function(){track(1)})
+leftSkip.addEventListener('click',function(){track(-1)})
+
+function track(switching){
+  indexArray = indexArray + switching
+}
+
 function playAudio() {
-  audio.src = 'music/bird.mp3';
   audio.currentTime = 0;
   audio.play();
 }
